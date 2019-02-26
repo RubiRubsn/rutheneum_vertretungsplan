dd3ba60a2686616917fdecb9943eff952f5da7f7
herrhotzenplotz <34663024+herrhotzenplotz@users.noreply.github.com>
2019-02-26 20:46:51 +0100

    First steps in implementing react hooks.
    Is not working yet.

---
+++ README.md
@@ -0,0 +1,8 @@
+# React-Native app für den Vertretungsplan des Rutheneums.
+
+## Notizen / Links
+* [React Hooks](https://reactjs.org/docs/hooks-state.html) (Für das Nachladen von States)
+* [Exception: (.., _react.useState) is not a function](https://stackoverflow.com/questions/53892733/react-16-7-typeerror-0-react-usestate-is-not-a-function)
+
+### Hinweis zu Link 1
+Offensichtlich ist eine höhere react Version nötig, um Hooks zu benutzen. --> expo muss höhere react Version unterstützen.
--- de.rutheneum-vertretungsplan/components/DisplayVPlan.js
+++ de.rutheneum-vertretungsplan/components/DisplayVPlan.js
@@ -1,71 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, SectionList, ScrollView, AsyncStorage } from 'react-native';
 import { Text, Divider, Overlay, Button } from 'react-native-elements';
 import { DataTable  } from 'react-native-paper';
-export class DisplayVPlan extends React.Component {
+export function DisplayVPlan() {
 
-    state = {
+    const [state, setState] = useState({
         Allgemein: '',
         Plan: [],
         isVisible: false,
         gestern:0
-    };
+    });
 
-    async componentDidMount() {
-        try
+    fetch(`https://rutheneumapi2.herokuapp.com/plan/${this.props.id}`)
+    .then(fetched => fetched.json())
+    .then(result => 
         {
-            await this.updateState();
-        }
-        catch{}
-    }
-
-
-    render(){
-        this.updateState();
-        return (
-            <ScrollView>
-                
-                <SectionList style={{backgroundColor: 'white'}} sections={this.state.Plan.map(lesson => ({ title: `${lesson.StundenNummer}. Stunde`, data: lesson.Content }))}
-                        renderItem={({item,index,section}) => (
-                            <View style={{ flex:1, flexDirection: "row" }} key={index}>
-                                
-                                    <View style={{ flex:1, flexDirection: "column" }}>
-                                        <Text>{item.Kurs}</Text>
-                                        <Divider style={{ height: 10, backgroundColor: 'white' }} />
-                                    </View>    
-                                    <View style={{ flex:1, flexDirection: "column" }}>
-                                        <Text >{item.Details}</Text>
-                                        <Divider style={{ height: 10, backgroundColor: 'white' }} />
-                                    </View>
-                                    
-                                
-                            </View>
-                        )}
-                        
-                        renderSectionHeader={item => (
-                            <View style={{ backgroundColor: '#d6d6d6' }}>
-                            <Divider style={{ height: 10, backgroundColor: 'white' }} />
-                            <Text style={{fontSize: 20}}>{item.section.title}</Text>
-                            <Divider style={{ height: 10, backgroundColor: 'white' }} />
-                            </View>
-                            )}
-                        keyExtractor={(item, index) => item + index}
-                        
-                />
-            </ScrollView>
-
-        );
-    
-}
-
-    async updateState(){
-        const fetched = await fetch(`https://rutheneumapi2.herokuapp.com/plan/${this.props.vplanid}`);
-        const jsonData = await fetched.json();
-
-        this.setState({
-            Allgemein: jsonData.Allgemein,
-            Plan: jsonData.Plan
+            setState(result);
         });
 
-    }
-}
+
+    
+    return (
+        <ScrollView>
+            
+            <SectionList style={{backgroundColor: 'white'}} sections={state.Plan.map(lesson => ({ title: `${lesson.StundenNummer}. Stunde`, data: lesson.Content }))}
+                    renderItem={({item,index,section}) => (
+                        <View style={{ flex:1, flexDirection: "row" }} key={index}>
+                            
+                                <View style={{ flex:1, flexDirection: "column" }}>
+                                    <Text>{item.Kurs}</Text>
+                                    <Divider style={{ height: 10, backgroundColor: 'white' }} />
+                                </View>    
+                                <View style={{ flex:1, flexDirection: "column" }}>
+                                    <Text >{item.Details}</Text>
+                                    <Divider style={{ height: 10, backgroundColor: 'white' }} />
+                                </View>
+                                
+                            
+                        </View>
+                    )}
+                    
+                    renderSectionHeader={item => (
+                        <View style={{ backgroundColor: '#d6d6d6' }}>
+                        <Divider style={{ height: 10, backgroundColor: 'white' }} />
+                        <Text style={{fontSize: 20}}>{item.section.title}</Text>
+                        <Divider style={{ height: 10, backgroundColor: 'white' }} />
+                        </View>
+                        )}
+                    keyExtractor={(item, index) => item + index}
+                    
+            />
+        </ScrollView>)
+        }
